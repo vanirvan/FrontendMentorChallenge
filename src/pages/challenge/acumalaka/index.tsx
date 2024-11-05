e8a62d9257f18dda81429615f5e501fdb18443d2
vanirvan <vanir1812@gmail.com>
2024-11-05 19:22:35 +0700

    test (page): create test page inside src/challenge

---
+++ src/pages/challenge/acumalaka/index.tsx
@@ -0,0 +1,12 @@
+export default function ReactProject() {
+  return (
+    <>
+      <span>Acumalaka</span>
+      <img
+        src={"/challenge/acumalaka/cover.jpg"}
+        alt="asd"
+        className="aspect-square w-72 object-cover"
+      />
+    </>
+  );
+}
