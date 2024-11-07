81f644e6d725f18d353318b37ec9642be13a6354
vanirvan <vanir1812@gmail.com>
2024-11-07 19:24:34 +0700

    feat: add more projects to display

--- src/data/project.ts
+++ src/data/project.ts
@@ -1,13 +1,54 @@
 // lists of all projects
 
 export interface Project {
-  url: string;
+  project_url: string;
+  frontendmentor_url: string;
   name: string;
   image: string;
   difficulty: 1 | 2 | 3 | 4 | 5;
-  finish_timestamp: number;
 }
 
-const projects: Project[] = [];
+const projects: Project[] = [
+  {
+    project_url: "/qr-code-component",
+    frontendmentor_url:
+      "https://www.frontendmentor.io/challenges/qr-code-component-iux_sIO_H",
+    name: "QR Code Component",
+    image: "/cover.png",
+    difficulty: 1,
+  },
+  {
+    project_url: "/blog-preview-card",
+    frontendmentor_url:
+      "https://www.frontendmentor.io/challenges/blog-preview-card-ckPaj01IcS",
+    name: "Blog Preview Card",
+    image: "/cover.png",
+    difficulty: 1,
+  },
+  {
+    project_url: "/mortgage-repayment-calculator",
+    frontendmentor_url:
+      "https://www.frontendmentor.io/challenges/mortgage-repayment-calculator-Galx1LXK73",
+    name: "Mortgage Repayment Calculator",
+    image: "/cover.png",
+    difficulty: 2,
+  },
+  {
+    project_url: "/bento-grid",
+    frontendmentor_url:
+      "https://www.frontendmentor.io/challenges/bento-grid-RMydElrlOj",
+    name: "Bento Grid",
+    image: "/cover.png",
+    difficulty: 2,
+  },
+  {
+    project_url: "/age-calculator-app",
+    frontendmentor_url:
+      "https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q",
+    name: "Age Calculator App",
+    image: "/cover.png",
+    difficulty: 2,
+  },
+];
 
 export default projects;
